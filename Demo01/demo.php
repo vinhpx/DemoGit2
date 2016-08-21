dd2a8177da53aa9b417e3839fa3b1b391456e2bc
Vinh Pham <phamxuanvinhdv@gmail.com>
2016-08-21 22:31:38 +0700

    First commit

---
+++ Demo01/demo.php
@@ -0,0 +1,13 @@
+
+<!DOCTYPE html>
+<html>
+<body>
+<p id="demo">Xin Chao Viet Nam</p>
+<script>
+var a= $('#demo').html();
+alert(a);
+</script>
+
+
+</body>
+</html>
---
+++ Demo01/signup.php
@@ -0,0 +1,7 @@
+<?php
+ $arr_cource = array("Android","IOS","Window");
+ 
+ $rand_keys=array_rand($arr_cource,1);
+ echo $arr_cource[$rand_keys];
+
+?>
